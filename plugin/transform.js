4ca5cff50f1e91ccea630039ab16c4b597b529bf
excuseme <excuseme@example.com>
2026-02-27 01:07:08 +0100

    Developing plugin

---
+++ plugin/transform.js
@@ -0,0 +1,9 @@
+function transform(input) {
+  return { data: {
+    me: input.IDX_0,
+    listening_sessions: input.IDX_1,
+    listening_stats: input.IDX_2,
+    items_in_progress: input.IDX_3
+  }
+  };
+}
